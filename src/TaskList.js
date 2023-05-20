528a3e21d90a2effc08bcbd411404a5cacc4179d
Milind Krishna <54195987+milindkrishna@users.noreply.github.com>
2023-05-20 09:45:02 +0530

    modified

---
+++ src/TaskList.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+function TaskList() {
+  const [taskInput, setTaskInput] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const handleInputChange = (e) => {
+    setTaskInput(e.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (taskInput.trim() !== '') {
+      setTasks([...tasks, { text: taskInput, completed: false }]);
+      setTaskInput('');
+    }
+  };
+
+  const handleToggleComplete = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <h1>Daily Task List</h1>
+      <input type="text" value={taskInput} onChange={handleInputChange} placeholder="Enter task" />
+      <button onClick={handleAddTask}>Submit</button>
+      <div>
+        {tasks.map((task, index) => (
+          <div key={index}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => handleToggleComplete(index)}
+            />
+            <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
+              {task.text}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TaskList;
